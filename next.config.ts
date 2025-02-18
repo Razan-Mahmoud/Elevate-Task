ab6b35e8ca42bf62a72667512f412d61c0b67a17
Razan M. Fahmi <razan_assem@hotmail.com>
2025-02-19 00:29:35 +0200

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/Elevate-Task",
+  basePath: "/elevate-task",
   output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+  
 };
 
 export default nextConfig;
